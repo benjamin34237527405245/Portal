5025419cf601a58c826ba6a71975866a498b61a4
RahulMakes <rawneedas@gmail.com>
2025-04-06 15:44:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-cache-v13'; // Update cache name to ensure new cache is used
+const CACHE_NAME = 'portal-cache-v14'; // Update cache name to ensure new cache is used
 const urlsToCache = [
     'index.html',
     'manifest.json',
